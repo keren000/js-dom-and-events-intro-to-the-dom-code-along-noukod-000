1fde084fecd624ecedda25f2f65f0b0f461a2935
Keren-Happuc Fleurimond <keren@noukod.com>
2019-02-28 14:50:43 +0000

    Automatically backed up by Learn

--- spec/index_spec.rb
+++ spec/index_spec.rb
@@ -1,10 +1,2 @@
 require 'pry'
-describe('index', () => {
-  describe('getFirstSelector(selector)', () => {
-    it('returns the first element that matches the selector', () => {
-      expect(getFirstSelector('div').id).toEqual('nested')
-      expect(getFirstSelector('.ranked-list')).toEqual(document.querySelector('.ranked-list'))
-    })
-  })
 
-)
