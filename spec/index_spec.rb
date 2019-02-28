79f018fd60f123c74b9baff0a85dc584881a3471
Keren-Happuc Fleurimond <keren@noukod.com>
2019-02-28 15:00:19 +0000

    Automatically backed up by Learn

--- spec/index_spec.rb
+++ spec/index_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'In this lab,' do
    context 'in index.html' do
 
      it 'contains a <body> tag' do
-      body = parsed_html.search('body')[0]
+      body = parsed_index.search('body')[0]
       expect(body).to_not be_nil, "No <body> tag was found"
     end
 
